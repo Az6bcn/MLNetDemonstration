7d546c20ded874e491b68e1cc1ae4488eb591e09
Michael Banner <michael.banner@codecomputerlove.com>
2020-01-15 21:18:16 +0000

    Initial commit

---
+++ MLNetConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
