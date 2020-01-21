d3098ffc6d6f287d604379e03dfc2b4243147847
Michael Banner <michael.banner@codecomputerlove.com>
2020-01-21 13:13:40 +0000

    Adding ONNX related assets and initial solution

---
+++ 02_Consuming_ONNX_Models/MLNetExample_ONNX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNetExample_ONNX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
