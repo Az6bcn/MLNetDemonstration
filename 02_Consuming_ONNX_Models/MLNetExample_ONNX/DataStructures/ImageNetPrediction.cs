e161bb23f14b8f23440557ef2f45b1cd2a160791
Michael Banner <michael.banner@codecomputerlove.com>
2020-01-28 08:50:39 +0000

    Adding core files

---
+++ 02_Consuming_ONNX_Models/MLNetExample_ONNX/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace MLNetExample_ONNX.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
---
+++ 02_Consuming_ONNX_Models/MLNetExample_ONNX/YoloParser/YoloBoundingBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLNetExample_ONNX.YoloParser
+{
+    using System.Drawing;
+
+    public class BoundingBoxDimensions : DimensionsBase { }
+
+    public class YoloBoundingBox
+    {
+        public BoundingBoxDimensions Dimensions { get; set; }
+
+        public string Label { get; set; }
+
+        public float Confidence { get; set; }
+
+        public RectangleF Rect
+        {
+            get { return new RectangleF(Dimensions.X, Dimensions.Y, Dimensions.Width, Dimensions.Height); }
+        }
+
+        public Color BoxColor { get; set; }
+    }
+}
